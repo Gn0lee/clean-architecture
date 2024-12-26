5a791a2a407218dff2f98fcd3e10520257c70462
Gn0lee <jinho605@gmail.com>
2024-12-26 09:59:10 +0900

    feat: fake 책 대여 서비스

--- src/services/bookAdaptor.ts
+++ src/services/bookAdaptor.ts
@@ -1,15 +1,40 @@
 import { BookService } from "../application/ports";
 import { Book } from "../domain/book";
+import { User } from "../domain/user";
 import { fakeApi } from "./api";
 import { faker } from "@faker-js/faker";
 
+const fakeCheckoutObject: Record<User["key"], Book[]> = {};
+
 export function useBook(): BookService {
   return {
-    checkoutBook: () => {
+    checkoutBook: (key, book) => {
+      if (!fakeCheckoutObject[key]) {
+        fakeCheckoutObject[key] = [];
+      } else {
+        const existingBook = fakeCheckoutObject[key].find(
+          (b) => b.id === book.id
+        );
+        if (!existingBook) {
+          fakeCheckoutObject[key].push(book);
+        } else {
+          return fakeApi(false);
+        }
+      }
+
       return fakeApi(true);
     },
-    returnBook: () => {
-      return fakeApi(true);
+    returnBook: (key, book) => {
+      const bookIndex = fakeCheckoutObject[key]?.findIndex(
+        (b) => b.id === book.id
+      );
+
+      if (bookIndex !== undefined || bookIndex !== -1) {
+        fakeCheckoutObject[key].splice(bookIndex, 1);
+        return fakeApi(true);
+      }
+
+      return fakeApi(false);
     },
     searchBook: () => {
       const fakeList: Book[] = Array.from({ length: 10 }).map(() => ({
@@ -21,8 +46,8 @@ export function useBook(): BookService {
 
       return fakeApi(fakeList);
     },
-    checkoutList: () => {
-      return fakeApi([]);
+    checkoutList: (key) => {
+      return fakeApi(fakeCheckoutObject[key] || []);
     },
   };
 }
